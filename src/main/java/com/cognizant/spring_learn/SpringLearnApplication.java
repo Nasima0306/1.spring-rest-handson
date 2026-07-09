8d77e27af0e647df4487a83ff1b1cc900f179693
demo <nasima365banu@gmail.com>
2026-07-09 09:19:13 +0530

    completed hands-on 4

--- src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,6 +1,8 @@
 package com.cognizant.spring_learn;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,32 +11,48 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 @SpringBootApplication
 public class SpringLearnApplication {
-    private static final Logger Logger= (Logger) LoggerFactory.getLogger(SpringLearnApplication.class);
-    private static final org.slf4j.Logger log = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
 
     public void displayDate() throws Exception {
 
-        Logger.info("START");
+        LOGGER.info("START");
 
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("date-format.xml");
 
         SimpleDateFormat format =
-                context.getBean("dateFormat",
-                        SimpleDateFormat.class);
+                context.getBean("dateFormat", SimpleDateFormat.class);
 
         Date date = format.parse("31/12/2018");
 
-        Logger.debug("Date = {}");
+        LOGGER.debug("Date = {}", date);
 
-        Logger.info("END");
+        LOGGER.info("END");
     }
 
-	public static void main(String[] args) {
+    public void displayCountry() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        LOGGER.debug("Country : {}", country);
+
+        LOGGER.info("END");
+    }
+
+    public static void main(String[] args) {
+
         try {
+
             SpringApplication.run(
                     SpringLearnApplication.class,
                     args);
@@ -43,10 +61,12 @@ public class SpringLearnApplication {
                     new SpringLearnApplication();
 
             app.displayDate();
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-	}
 
+            app.displayCountry();
+
+        } catch (Exception e) {
+
+            LOGGER.error("Exception occurred", e);
+        }
+    }
 }
