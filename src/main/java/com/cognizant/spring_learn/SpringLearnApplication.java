63b0b82cf050410ee266f748ef8a1e9eb6a8250a
demo <nasima365banu@gmail.com>
2026-07-09 09:12:28 +0530

    hands-on 3 completed

--- src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,5 +1,6 @@
 package com.cognizant.spring_learn;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,9 +9,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 @SpringBootApplication
 public class SpringLearnApplication {
+    private static final Logger Logger= (Logger) LoggerFactory.getLogger(SpringLearnApplication.class);
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(SpringLearnApplication.class);
+
     public void displayDate() throws Exception {
+
+        Logger.info("START");
 
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("date-format.xml");
@@ -21,7 +28,9 @@ public class SpringLearnApplication {
 
         Date date = format.parse("31/12/2018");
 
-        System.out.println(date);
+        Logger.debug("Date = {}");
+
+        Logger.info("END");
     }
 
 	public static void main(String[] args) {
